1306b1f265915e3e0d81cfd492a68910585086c2
aboutamirhamza <amirhamzashuvokhan@gmail.com>
2025-05-29 17:09:36 +0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,36 @@ import Button from "./Components/Button";
 
 function App() {
   
-  const [count,setCount] = useState(0);
+  // const [count,setCount] = useState(0);
+
+  // let isDisable = true;
 
   return (
     <>
       {/* <h1><Header title="My Count" /> {count}</h1>
       <button onClick={()=> setCount(count + 1)}>Add</button> */}
 
-      <Display show={count} />
-      <Button count={count} setCount={setCount} />
+      {/* <Display show={count} />
+      <Button count={count} setCount={setCount} /> */}
+
+      {/* {isDisable ? "Yes True" : "No False"}
+
+      {console.log(isDisable)} */}
+
+      {/* {isDisable && "Yes Yes And And True"}
+
+      {console.log(isDisable)} */}
+
+      {/* <button onClick={()=> console.log("Click From JSX React")}>Click Me JSX From React</button> */}
+
+      {/* <button class="btn">Click Me JS</button>
+    <script>
+      let btn = document.querySelector(".btn");
+      btn.addEventListener("click",()=>{
+        console.log("Click From Vanilla JS");
+      })
+    </script> */}
+
     </>
   )
 }
